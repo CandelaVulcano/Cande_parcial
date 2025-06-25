ada780aed6c2083aaf96988121ebec8c77c982fe
Alejandro Raúl Ruiz Andreola <alerruiz2004@gmail.com>
2025-06-25 18:33:39 -0300

    trabajo en clase 25/06 - implementación de generacion de archivos

--- app/resources/certificado_resources.py
+++
@@ -1,6 +0,0 @@
-from flask import Bluesprint
-
-certificado_bp = Blueprint('certificado', __name__)
-@certificado_bp.route('certificado', __name__)
-def reporte_odt():
-    pass
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,6 @@
 from .facultad_service import FacultadService
+from .alumno_service import AlumnoService
+from .materia_service import MateriaService
+from .documentos_office_service import PDFDocument, ODTDocument, DOCXDocument
+
+
