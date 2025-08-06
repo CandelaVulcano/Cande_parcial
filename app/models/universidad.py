104526e44bc17f027d77bd0e4184acab3d895a29
CandelaVulcano <candelavul@gmail.com>
2025-08-06 17:57:53 -0300

    hash

--- app/models/universidad.py
+++ app/models/universidad.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
 from app import db
+from flask_hashids import HashidMixmin
+
 
 @dataclass(init=False, repr=True, eq=True)
-class Universidad(db.Model):
+class Universidad(HashidMixmin, db.Model):
     __tablename__ = 'universidades'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False)
     sigla = db.Column(db.String(10), nullable=False)
-    facultades = db.relationship('Facultad', back_populates='universidad', lazy=True)
+    facultades = db.relationship(
+        'Facultad', back_populates='universidad', lazy=True)
