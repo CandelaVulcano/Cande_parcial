bd86576da27889b860943a74e50ee960fed1de64
CandelaVulcano <candelavul@gmail.com>
2025-08-09 13:50:29 -0300

    mas mmappppeoooo

--- app/models/grupo.py
+++ app/models/grupo.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
 from app import db
 from app.models.alumno_grupo import alumno_grupo
+from flask_hashids import HashidMixmin
+
 
 @dataclass(init=False, repr=True, eq=True)
-class Grupo(db.Model):
+class Grupo(HashidMixmin, db.Model):
     __tablename__ = 'grupos'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False)
-    alumnos = db.relationship('Alumno', secondary=alumno_grupo, back_populates='grupos')
+    alumnos = db.relationship(
+        'Alumno', secondary=alumno_grupo, back_populates='grupos')
--- app/models/orientacion.py
+++ app/models/orientacion.py
@@ -1,14 +1,19 @@
 from dataclasses import dataclass
 from app import db
+from flask_hashids import HashidMixmin
+
 
 @dataclass(init=False, repr=True, eq=True)
-class Orientacion(db.Model):
+class Orientacion(HashidMixmin, db.Model):
     __tablename__ = 'orientaciones'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False)
-    especialidad_id = db.Column(db.Integer, db.ForeignKey('especialidades.id'), nullable=False)
+    especialidad_id = db.Column(db.Integer, db.ForeignKey(
+        'especialidades.id'), nullable=False)
     plan_id = db.Column(db.Integer, db.ForeignKey('planes.id'), nullable=False)
-    materia_id = db.Column(db.Integer, db.ForeignKey('materias.id'), nullable=False)
-    especialidad = db.relationship('Especialidad', back_populates='orientaciones')
+    materia_id = db.Column(db.Integer, db.ForeignKey(
+        'materias.id'), nullable=False)
+    especialidad = db.relationship(
+        'Especialidad', back_populates='orientaciones')
     plan = db.relationship('Plan', back_populates='orientaciones')
     materia = db.relationship('Materia', back_populates='orientaciones')
