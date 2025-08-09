1de5c25c4203c67663d4a2e40bb41a11c0ab896a
CandelaVulcano <candelavul@gmail.com>
2025-08-09 11:15:10 -0300

    mapping

--- app/models/departamento.py
+++ app/models/departamento.py
@@ -1,12 +1,16 @@
 from dataclasses import dataclass
 from app import db
+from flask_hashids import HashidMixmin
+
 
 @dataclass(init=False, repr=True, eq=True)
-class Departamento(db.Model):
+class Departamento(HashidMixmin, db.Model):
     __tablename__ = 'departamentos'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False)
-    facultad_id = db.Column(db.Integer, db.ForeignKey('facultades.id'), nullable=False)
+    facultad_id = db.Column(db.Integer, db.ForeignKey(
+        'facultades.id'), nullable=False)
     facultad = db.relationship('Facultad', back_populates='departamentos')
-    materias = db.relationship('Materia', back_populates='departamento', lazy=True)
+    materias = db.relationship(
+        'Materia', back_populates='departamento', lazy=True)
     area_id = db.Column(db.Integer, db.ForeignKey('areas.id'), nullable=True)
--- app/models/especialidad.py
+++ app/models/especialidad.py
@@ -1,13 +1,18 @@
 from dataclasses import dataclass
 from app import db
+from flask_hashids import HashidMixmin
+
 
 @dataclass(init=False, repr=True, eq=True)
-class Especialidad(db.Model):
+class Especialidad(HashidMixmin, db.Model):
     __tablename__ = 'especialidades'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False)
     letra = db.Column(db.String(5), nullable=True)
     observacion = db.Column(db.String(200), nullable=True)
-    tipo_especialidad_id = db.Column(db.Integer, db.ForeignKey('tipos_especialidad.id'), nullable=False)
-    tipo_especialidad = db.relationship('TipoEspecialidad', back_populates='especialidades')
-    orientaciones = db.relationship('Orientacion', back_populates='especialidad', lazy=True)
+    tipo_especialidad_id = db.Column(db.Integer, db.ForeignKey(
+        'tipos_especialidad.id'), nullable=False)
+    tipo_especialidad = db.relationship(
+        'TipoEspecialidad', back_populates='especialidades')
+    orientaciones = db.relationship(
+        'Orientacion', back_populates='especialidad', lazy=True)
