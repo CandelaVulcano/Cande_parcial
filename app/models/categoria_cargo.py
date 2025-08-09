fd499a793ea2c6602f02b66199aac800e94aa722
CandelaVulcano <candelavul@gmail.com>
2025-08-09 13:42:42 -0300

    facultad

--- app/models/categoria_cargo.py
+++ app/models/categoria_cargo.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
 from app import db
+from flask_hashids import HashidMixmin
+
 
 @dataclass(init=False, repr=True, eq=True)
-class CategoriaCargo(db.Model):
+class CategoriaCargo(HashidMixmin, db.Model):
     __tablename__ = 'categorias_cargo'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False)
-    cargos = db.relationship('Cargo', back_populates='categoria_cargo', lazy=True)
+    cargos = db.relationship(
+        'Cargo', back_populates='categoria_cargo', lazy=True)
--- app/models/facultad.py
+++ app/models/facultad.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 from app import db
+from flask_hashids import HashidMixmin
+
 
 @dataclass(init=False, repr=True, eq=True)
-class Facultad(db.Model):
+class Facultad(HashidMixmin, db.Model):
     __tablename__ = 'facultades'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False)
@@ -15,7 +17,10 @@ class Facultad(db.Model):
     telefono = db.Column(db.String(20), nullable=True)
     contacto = db.Column(db.String(100), nullable=True)
     email = db.Column(db.String(100), nullable=False)
-    universidad_id = db.Column(db.Integer, db.ForeignKey('universidades.id'), nullable=False)
+    universidad_id = db.Column(db.Integer, db.ForeignKey(
+        'universidades.id'), nullable=False)
     universidad = db.relationship('Universidad', back_populates='facultades')
-    autoridades = db.relationship('Autoridad', back_populates='facultad', lazy=True)
-    departamentos = db.relationship('Departamento', back_populates='facultad', lazy=True)
+    autoridades = db.relationship(
+        'Autoridad', back_populates='facultad', lazy=True)
+    departamentos = db.relationship(
+        'Departamento', back_populates='facultad', lazy=True)
