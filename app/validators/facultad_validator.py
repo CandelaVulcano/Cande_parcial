c5b21a312b7d7758e5ef5217f0330615ddf5e732
GiulianaBettiol <giubettiolriva@gmail.com>
2025-08-24 19:38:30 -0300

    Validators

--- app/validators/facultad_validator.py
+++ app/validators/facultad_validator.py
@@ -4,5 +4,5 @@ def validate_facultad(data):
         errors.append('El nombre de la facultad es obligatorio.')
     if not data.get('codigo'):
         errors.append('El código de la facultad es obligatorio.')
-    # Agrega más validaciones según tu modelo
+    
     return errors
--- app/validators/tipo_documento_validator.py
+++ app/validators/tipo_documento_validator.py
@@ -1,12 +1,8 @@
-
-class AlumnoValidator:
+class TipoDocumentoValidator:
 
     @staticmethod
-    def validate_alumno(data):
+    def validate_tipo_documento(data):
         errors = []
         if not data.get('nombre'):
             errors.append('El nombre es obligatorio.')
-        if not data.get('dni'):
-            errors.append('El DNI es obligatorio.')
-        
         return errors
