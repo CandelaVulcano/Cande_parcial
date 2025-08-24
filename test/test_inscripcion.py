dda47cdd7139c249f55619ec29a1b1a876ce0952
Alejandro Raúl Ruiz Andreola <alerruiz2004@gmail.com>
2025-08-24 18:31:59 -0300

    creación de test de inscripcion
    se creó el test de inscripción

---
+++ test/test_inscripcion.py
@@ -0,0 +1,75 @@
+import unittest
+import os
+import datetime
+from flask import current_app
+from app import create_app, db
+from app.models.inscripcion import Inscripcion
+from app.services.inscripcion_service import InscripcionService
+from app.repositories.inscripcion_repositorio import InscripcionRepository
+
+class InscripcionTestCase(unittest.TestCase):
+    def setUp(self):
+        os.environ['FLASK_CONTEXT'] = 'testing'
+        self.app = create_app()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def __nuevaInscripcion(self, alumno_id=1, materia_id=1, fecha=None):
+        ins = Inscripcion()
+        ins.alumno_id = alumno_id
+        ins.materia_id = materia_id
+        ins.fecha = fecha or datetime.date.today()
+        return ins
+
+    def test_inscripcion_creation(self):
+        ins = self.__nuevaInscripcion()
+        self.assertIsNotNone(ins)
+        self.assertEqual(ins.alumno_id, 1)
+        self.assertEqual(ins.materia_id, 1)
+        self.assertIsNotNone(ins.fecha)
+
+    def test_crear_inscripcion(self):
+        ins = self.__nuevaInscripcion()
+        InscripcionService.crear(ins)
+        self.assertIsNotNone(ins.id)
+        encontrada = InscripcionService.buscar_por_id(ins.id)
+        self.assertIsNotNone(encontrada)
+        self.assertEqual(encontrada.alumno_id, ins.alumno_id)
+
+    def test_buscar_todos_inscripciones(self):
+        ins1 = self.__nuevaInscripcion(alumno_id=1, materia_id=1)
+        ins2 = self.__nuevaInscripcion(alumno_id=2, materia_id=2)
+        InscripcionService.crear(ins1)
+        InscripcionService.crear(ins2)
+        todas = InscripcionService.buscar_todos()
+        self.assertIsNotNone(todas)
+        self.assertEqual(len(todas), 2)
+        self.assertIn(ins1, todas)
+        self.assertIn(ins2, todas)
+
+    def test_actualizar_inscripcion(self):
+        ins = self.__nuevaInscripcion(alumno_id=1, materia_id=1)
+        InscripcionService.crear(ins)
+        ins.alumno_id = 42
+        actualizado = InscripcionService.actualizar(ins)
+        self.assertIsNotNone(actualizado)
+        self.assertEqual(actualizado.alumno_id, 42)
+        desde_db = InscripcionService.buscar_por_id(ins.id)
+        self.assertEqual(desde_db.alumno_id, 42)
+
+    def test_borrar_inscripcion(self):
+        ins = self.__nuevaInscripcion()
+        InscripcionService.crear(ins)
+        borrado = InscripcionService.borrar_por_id(ins.id)
+        self.assertIsNotNone(borrado)
+        encontrado = InscripcionService.buscar_por_id(ins.id)
+        self.assertIsNone(encontrado)
+
+if __name__ == '__main__':
+    unittest.main()
