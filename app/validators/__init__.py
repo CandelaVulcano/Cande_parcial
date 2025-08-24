36897523136786ea443cd374a776427a37cb0726
CandelaVulcano <candelavul@gmail.com>
2025-08-24 12:42:28 -0300

    aaaaa

--- app/validators/__init__.py
+++ app/validators/__init__.py
@@ -1,2 +1,2 @@
 from .alumno_validator import AlumnoValidator
-from .validate_with import validate_with
+from .departamento_validator import DepartamentoValidator
--- app/validators/alumno_validator.py
+++ app/validators/alumno_validator.py
@@ -1,8 +1,12 @@
-def validate_alumno(data):
-    errors = []
-    if not data.get('nombre'):
-        errors.append('El nombre es obligatorio.')
-    if not data.get('dni'):
-        errors.append('El DNI es obligatorio.')
-    # Agrega más validaciones según tu modelo
-    return errors
+
+class AlumnoValidator:
+
+    @staticmethod
+    def validate_alumno(data):
+        errors = []
+        if not data.get('nombre'):
+            errors.append('El nombre es obligatorio.')
+        if not data.get('dni'):
+            errors.append('El DNI es obligatorio.')
+        # Agrega más validaciones según tu modelo
+        return errors
